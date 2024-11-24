dd6025bdee98b5a15945cdf4a0e73774dc38ce6b
cesarpatriciovm <cesarvalladaresmilla@gmail.com>
2024-11-24 12:06:22 -0300

    pendiente mejoras css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 // import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Home from './pages/Homes'
 
 function App() {
   
   return (
     <>
-      
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
