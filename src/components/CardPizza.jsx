07a170f1d452ed6fb0eb4badd0d7d87f86c64102
cesarpatriciovm <cesarvalladaresmilla@gmail.com>
2024-11-24 23:22:24 -0300

    version enviada

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -5,12 +5,14 @@ function CardPizza (props) {
   return (
     <div className="cardpizza__infopizza">
       <header className="infopizza__header">
-        <img className="header__img" src={props.img} alt="" />
+        <figure className='header__container-img'>
+          <img className="header__img" src={props.img} alt="" />
+        </figure>
         <h3 className="header__title">{props.name}</h3>
       </header>
       <main className="infopizza__main">
         <h4 className="main__title">Ingredientes:</h4>
-        <p className="main__ing">🍕{props.ingredients}</p>
+        <p className="main__ing">🍕{props.ingredients.join(', ')}</p>
       </main>   
       <footer className="infopizza__footer">
         <h4 className="footer__title">Precio: ${formatCurr(props.price)}</h4>
